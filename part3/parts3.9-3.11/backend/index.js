0e6d9b518dfeabafbb9700a31da2fdba0d1fad4b
Chathurangi <edussuriya.c@eng.pdn.ac.lk>
2022-11-16 23:23:14 +0200

    add heroku

--- part3/parts3.9-3.11/backend/index.js
+++ part3/parts3.9-3.11/backend/index.js
@@ -121,7 +121,6 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   })
   
-
   const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
