e6478643600d3ef6266bbbc13c485572cdac9d03
Chathurangi <edussuriya.c@eng.pdn.ac.lk>
2022-11-16 23:02:17 +0200

    add part1, part2, part3

---
+++ part2/Tasks2.11-2.14/part2/src/App.js
@@ -0,0 +1,143 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const api_key = process.env.REACT_APP_API_KEY 
+
+const Filter = ({searchString,handleSearchStringChange}) => {
+  return (
+    <div>
+      find countries <input value={searchString} onChange={handleSearchStringChange}/>
+    </div>
+  )
+}
+
+const CountryName = ({country}) => {
+  const [visible, setVisible] = useState(false)
+  return (
+    <div>
+      {country.name.common}
+      <button onClick={() => setVisible(true)}>
+        show
+       </button>
+       {visible && <CountryShowing country={country} />}
+    </div>
+  )
+}
+
+const CountryShowing = ({country}) => {
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      capital {country.capital[0]}
+      <br/>
+      area {country.area}
+      <h3> languages</h3>
+      
+      {Object.values(country.languages).map((lang) => 
+        <li key={lang}>{lang}</li>)}
+      <h1>
+      {country.flag}</h1>
+      <Weather city={country.capital[0]}/>
+      <br/>
+    </div>
+  )
+}
+
+const Countries = ({searchString, countriesToShow}) => {
+  console.log(countriesToShow)
+  if (searchString==='') {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+  if (countriesToShow.length>10) {
+    return (
+      <div>
+      'Too many matches, specify another filter' 
+      </div>
+    )
+  } 
+
+  return (
+    <div>
+      {countriesToShow.map(country=>
+        <CountryName key={country.tld[0]} country = {country}/>)}
+   
+    </div>
+  )
+}
+//**************************************remember to remove api key */
+const Weather = ({city}) => {
+  const [weather, setWeather] = useState([])
+  const url='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f4050762386e4176f479181d96cda1da'
+  const hook = () => {
+    axios
+      .get(url) // api key remove ************
+      .then(response => {
+        console.log(response.data)
+        setWeather(response.data)
+      })
+  }
+  useEffect(hook, [])
+  
+  if (weather.length!=0){
+  const icon = weather.weather[0].icon
+  // const icon = '10d'
+  const url_icon = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
+  return (
+    <div>
+      <h2> Weather in {city}</h2>
+      <p>temperature {weather.main.temp} celcius</p>
+      <img src={url_icon} />
+      <p>wind {weather.wind.speed} m/s </p>
+    
+    </div>
+  )
+  } else {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchString, setSearchString] = useState('')
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        
+        setCountries(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  const handleSearchStringChange = (event) => {   
+    setSearchString(event.target.value)
+  }
+
+  const countriesToShow = countries.filter(country=> country.name.common.toLowerCase().includes(searchString))
+  
+
+  return (
+    <div>
+      <Filter searchString={searchString} handleSearchStringChange={handleSearchStringChange}/>
+      <Countries searchString={searchString} countriesToShow={countriesToShow}/>
+    
+    </div>
+  )
+
+}
+
+
+export default App
+
---
+++ part3/parts3.9-3.11/frontend/src/App.js
@@ -0,0 +1,197 @@
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+const Person = (props) => {
+ 
+  return (
+    <div>
+     {props.name} {props.number} 
+     <button onClick={()=> deletePerson(props)}>
+        delete
+       </button>
+    </div>
+  )
+}
+
+const deletePerson = (props) => {
+  const alert = 'Delete ' + props.name + ' ?'
+  if (window.confirm(alert) ){
+    personService
+    .deletePerson(props.id)
+      .then(()=> props.setPersons(props.persons.filter((person => person.id!==props.id)))).catch(error=>{
+        console.log('error')
+        props.setError('Information of '+ props.name+' has already been removed from the server')
+      })
+    props.setMessage('Deleted ' +props.name)
+    setTimeout(() => {props.setMessage(null)
+    }, 5000)
+  }
+}
+
+const Filter = ({searchString,handleSearchStringChange}) => {
+  return (
+    <div>
+      filter shown with <input value={searchString} onChange={handleSearchStringChange}/>
+    </div>
+  )
+}
+
+const PersonForm = ({addPerson,newName,handlePersonChange,newNumber,handleNumberChange}) => {
+  return (
+    <div>
+       <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handlePersonChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
+      </form>
+    </div>
+  )
+}
+
+const updatePerson = (personObject, persons, setPersons, setNewName, setNewNumber,setMessage, setError) => {
+  console.log(personObject)
+  const per=persons.find(person=> person.name===personObject.name)
+   
+  const alert =  personObject.name + ' is already added to phonebook, replace the old number with a new one?'
+  if (window.confirm(alert) ){
+    
+    const changePerson={...per,number:personObject.number}
+    personService
+    .update(per.id, changePerson)
+    .then((returnedPerson) => {
+      console.log(returnedPerson)
+      setPersons(persons.map(person => person.id!==per.id? person:returnedPerson.data))
+      setNewName('')
+      setNewNumber('')
+      setMessage('Updated '+personObject.name)
+      setTimeout(() => {          setMessage(null)
+      }, 5000)
+    }).catch(error=>{
+      console.log('error')
+      setError('Information of '+ personObject.name+' has already been removed from the server')
+      setTimeout(() => {          setError(null)
+      }, 5000)
+    }) 
+    }
+}
+
+const Persons = ({personsToShow, setPersons, persons, setMessage, setError}) => {
+  
+  return (
+    <div>
+      {personsToShow.map(personObject => 
+          <Person key={personObject.id} name={personObject.name} number ={personObject.number} persons={persons} setPersons={setPersons} id={personObject.id} setMessage={setMessage} setError={setError}/>
+        )}
+    </div>
+  )
+}
+
+const Notification = ({ message }) => {
+ 
+  if (message===null || message.length === 0 ) { 
+    return null
+  }
+
+  return (
+    <div className='message'>
+      {message}
+    </div>
+  )
+}
+
+const NotificationError = ({ error }) => {
+ 
+  if (error===null || error.length === 0 ) { 
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {error}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([ { name: 'Arto Hellas', number: '040-123456', id: '1'},
+  { name: 'Ada Lovelace', number: '39-44-5323523', id: '2'},
+  { name: 'Dan Abramov', number: '12-43-234345', id: '3' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122', id: '4' }]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchString, setSearchString] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log(persons)
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+    
+    persons.find(person=> person.name===newName) ? 
+       updatePerson (personObject, persons, setPersons, setNewName, setNewNumber, setMessage, setError): 
+        personService
+        .create(personObject)
+        .then(() => {
+          setPersons(persons.concat(personObject))
+          setNewName('')
+          setNewNumber('')
+          setMessage('Added '+newName)
+          setTimeout(() => {          setMessage(null)
+          }, 5000)
+        }).catch(error=>{
+          console.log('error')
+        })
+   
+  }
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const personsToShow = searchString === ''?
+      persons: persons.filter(person=> person.name.includes(searchString))
+  
+    
+  const handlePersonChange = (event) => {
+   
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchStringChange = (event) => {
+   
+    setSearchString(event.target.value)
+  }
+
+
+  return (
+    <div>
+      
+      <h2>Phonebook</h2>
+      <Notification message={message}/>
+      <NotificationError error={error}/>
+      <Filter searchString={searchString} handleSearchStringChange={handleSearchStringChange}/>
+      <h3>add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} handlePersonChange={handlePersonChange} 
+      newNumber={newNumber} handleNumberChange={handleNumberChange} setMessage={setMessage}/>
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} persons={persons} setPersons={setPersons} setMessage={setMessage} setError={setError}/>
+    </div>
+  )
+}
+
+export default App
