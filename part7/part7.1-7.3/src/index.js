354b98255840ed9ead9aee36cecff33dd0c1a89f
Chathurangi <edussuriya.c@eng.pdn.ac.lk>
2022-12-02 11:23:03 +0200

    add useField 7.6

---
+++ part7/part7.1-7.3/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+    BrowserRouter as Router,
+    Routes, Route, Link,useMatch
+  } from "react-router-dom"
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
